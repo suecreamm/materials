07b619c1c1418341b1dd6168b905e61611d17abc
suecreamm <100839777+suecreamm@users.noreply.github.com>
2024-08-30 10:07:00 +0900

    Update B001_phononJobAuto.sh

--- B001_phononJobAuto.sh
+++ B001_phononJobAuto.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+  
 job_ids=()
 
 #source /opt/intel_2021/oneapi/setvars.sh
@@ -10,14 +9,14 @@ cp ../INCAR .
 sed -i '/ISIF/d; /IBRION/d; /EDIFF/d; /IALGO/d; /LREAL/d; /LWAVE/d; /LCHARGE/d; /PREC/d; /ISMEAR/d; /SIGMA/d;' INCAR
 
 sed -i '$a\
-PREC = Accurate\
-IBRION = -1\
-EDIFF = 1e-08\
-ISMEAR = 0; SIGMA = 0.01;\
-IALGO = 38\
-LREAL = .FALSE.\
-LWAVE = .FALSE.\
-LCHARG = .FALSE.\' INCAR
+PREC = Accurate;
+IBRION = -1;
+EDIFF = 1e-08;
+ISMEAR = 0; SIGMA = 0.01;
+IALGO = 38;
+LREAL = .FALSE.;
+LWAVE = .FALSE.;
+LCHARG = .FALSE.;' INCAR
 
 cp ../KPOINTS .
 sed -i '$d' KPOINTS
@@ -27,22 +26,22 @@ cp ../POTCAR .
 cp ../queue_script .
 
 phonopy -d --dim 2 2 2 --pa auto -c POSCAR
-  
+
 for m in `ls POSCAR-0*`
 do
-	fol=`echo $m|cut -d "-" -f 2`
-	mkdir disp_$fol
-	cp POSCAR-$fol disp_$fol/POSCAR
-	cp INCAR disp_$fol/INCAR
-	cp POTCAR disp_$fol/POTCAR
-	cp KPOINTS disp_$fol/KPOINTS
-	cp queue_script disp_$fol/queue_script
+        fol=`echo $m|cut -d "-" -f 2`
+        mkdir disp_$fol
+        cp POSCAR-$fol disp_$fol/POSCAR
+        cp INCAR disp_$fol/INCAR
+        cp POTCAR disp_$fol/POTCAR
+        cp KPOINTS disp_$fol/KPOINTS
+        cp queue_script disp_$fol/queue_script
 
-	cd disp_$fol/
-	job_id=$(qsub queue_script)
-	job_ids+=("$job_id")
-	echo "Submitted JobID: $job_id" > ~/last_phonopyjob.txt
-	cd ..
+        cd disp_$fol/
+        job_id=$(qsub queue_script)
+        job_ids+=("$job_id")
+        echo "Submitted JobID: $job_id" > ~/last_phonopyjob.txt
+        cd ..
 
 done
 
